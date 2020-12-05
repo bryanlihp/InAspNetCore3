05f86c61286b038f0964b505324315e0053ffc19
bryan.li@burli.com <bryan.li@burli.com>
2020-12-05 12:04:44 -0800

    ConfigLoader

---
+++ Confguration/ConfigLoader.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Configuration.Options;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Memory;
+
+namespace Configuration
+{
+    public static class ConfigLoader
+    {
+        public static void LoadOptionsFromConfigSource()
+        {
+
+            ConfigLoader.MemorySource();
+            ConfigLoader.FileSource();
+            ConfigLoader.EnvironmentConfiguration("develop");
+            ConfigLoader.EnvironmentConfiguration("staging");
+            ConfigLoader.FileSource(true);
+        }
+
+        private static void MemorySource()
+        {
+            Console.WriteLine("Load format options from memory key-value pairs");
+            var source = new Dictionary<string, string>
+            {
+                ["Format:Datetime:LongDatePattern"] = "dddd, MMM d, yyyy",
+                ["Format:Datetime:LongTimePattern"] = "h:mm:ss tt",
+                ["Format:Datetime:ShortDatePattern"] = "M/d/yyyy",
+                ["Format:Datetime:ShortTimePattern"] = "h:mm tt",
+
+                ["Format:CurrencyDecimal:Digits"] = "2",
+                ["Format:CurrencyDecimal:Symbol"] = "$",
+            };
+
+            var configuration = new ConfigurationBuilder()
+                .Add(new MemoryConfigurationSource { InitialData = source })
+                .Build();
+            LoadFormatOptions(configuration);
+            Console.WriteLine();
+
+
+        }
+        private static void FileSource(bool reloadOnChange = false)
+        {
+            Console.WriteLine("Load format options from appsettings.json");
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile(path: "appsettings.json", optional: false, reloadOnChange: reloadOnChange)
+                .Build();
+            LoadFormatOptions(configuration, false);
+            Console.WriteLine("Update appsettings.json.");
+            if (reloadOnChange)
+            {
+                Console.Read();
+                LoadFormatOptions(configuration, false);
+                Console.WriteLine("Reloaded.");
+                Console.Read();
+            }
+            Console.WriteLine();
+        }
+
+        private static void EnvironmentConfiguration(string environment)
+        {
+            Console.WriteLine($"Load format options - env ={environment}");
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", false)
+                .AddJsonFile($"appsettings.{environment}.json", true)
+                .Build();
+            LoadFormatOptions(configuration, false);
+            Console.WriteLine();
+        }
+
+        private static void LoadFormatOptions(IConfigurationRoot configuration, bool testAll = true)
+        {
+            var formatSection = configuration.GetSection("Format");
+            if (testAll)
+            {
+                var options1 = new FormatOptions(formatSection);
+                Console.WriteLine("Show options1");
+                ShowOptions(options1);
+            }
+            var options2 = formatSection.Get<FormatOptions>();
+            Console.WriteLine("Show options2");
+            ShowOptions(options2);
+        }
+
+        private static void ShowOptions(FormatOptions options)
+        {
+            var dateTime = options.DateTime;
+            var currencyDecimal = options.CurrencyDecimal;
+
+            Console.WriteLine("DateTime:");
+            Console.WriteLine($"\tLongDatePattern: {dateTime.LongDatePattern}");
+            Console.WriteLine($"\tLongTimePattern: {dateTime.LongTimePattern}");
+            Console.WriteLine($"\tShortDatePattern: {dateTime.ShortDatePattern}");
+            Console.WriteLine($"\tShortTimePattern: {dateTime.ShortTimePattern}");
+
+            Console.WriteLine("CurrencyDecimal:");
+            Console.WriteLine($"\tDigits:{currencyDecimal.Digits}");
+            Console.WriteLine($"\tSymbol:{currencyDecimal.Symbol}");
+        }
+    }
+}
---
+++ Confguration/Options/FormatOptions.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+
+namespace Configuration.Options
+{
+    public class DateTimeFormatOptions
+    {
+        public DateTimeFormatOptions()
+        {
+
+        }
+        public DateTimeFormatOptions(IConfiguration config)
+        {
+            LongDatePattern = config["LongDatePattern"];
+            LongTimePattern = config["LongTimePattern"];
+            ShortDatePattern = config["ShortDatePattern"];
+            ShortTimePattern = config["ShortTimePattern"];
+        }
+        public string LongDatePattern { get; set; }
+        public string LongTimePattern { get; set; }
+        public string ShortDatePattern { get; set; }
+        public string ShortTimePattern { get; set; }
+    }
+
+    public class CurrencyDecimalFormatOptions
+    {
+        public int Digits { get; set; }
+        public string Symbol { get; set; }
+        public CurrencyDecimalFormatOptions(IConfiguration config)
+        {
+            Digits = int.Parse(config["Digits"]);
+            Symbol = config["Symbol"];
+        }
+
+        public CurrencyDecimalFormatOptions()
+        {
+
+        }
+    }
+
+    public class FormatOptions
+    {
+        public FormatOptions()
+        {
+        }
+
+        public FormatOptions(IConfiguration config)
+        {
+            DateTime = new DateTimeFormatOptions(config.GetSection("DateTime"));
+            CurrencyDecimal = new CurrencyDecimalFormatOptions(config.GetSection("CurrencyDecimal"));
+        }
+        public DateTimeFormatOptions DateTime { get; set; }
+        public CurrencyDecimalFormatOptions CurrencyDecimal { get; set; }
+    }
+
+}
